9a25df17bfc447c4750d2572a060370d43a06086
Saralune <sarahlef59@hotmail.fr>
2022-06-08 16:18:09 +0200

    add business package

--- SpringShopJpa/src/main/java/fr/fms/MyShopApp.java
+++ SpringShopJpa/src/main/java/fr/fms/MyShopApp.java
@@ -1,75 +1,152 @@
 package fr.fms;
 
+import java.util.Collections;
 import java.util.Scanner;
 
-public class MyShopApp {
-	private static Scanner scan = new Scanner(System.in);
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import fr.fms.business.IMyShopAppImpl;
+
+@SpringBootApplication
+public class MyShopApp {	
+	private static Scanner scan = new Scanner(System.in);
+	private static IMyShopAppImpl business = new IMyShopAppImpl();
+	
 	public static void main(String[] args) {
-		System.out.println("Bonjour et bienvenue sur notre application de vente de livres neufs et d'occasion.");
+		SpringApplication.run(MyShopApp.class, args);
 		
+		System.out.println("Bonjour.");
+
 		int choice = 0;
-		while(choice != 7) {
+		while (choice != 6) {
 			displayMenu();
-			
+
 			choice = scanInt();
-			
-			switch(choice) {
-				case 1:
-					
-					break;
-					
-				case 2:
-					
-					break;
-					
-				case 3: 
-					
-					break;
-					
-				case 4:
-					
-					break;
-					
-				case 5:
-					
-					break;
-					
-				case 6:
-					
-					break;
-					
-				case 7:
-					System.out.println("Au revoir !");
-					break;
+
+			switch (choice) {
+			case 1:
+				displayArticles();
+				break;
+
+			case 2:
+				displayArticlesByPage();
+				break;
+
+			case 3:
+				menuArticles();
+				break;
+
+			case 4:
+				displayCRUDMenu("une catégorie");
+				break;
+
+			case 5:
+
+				break;
+
+			case 6:
+				System.out.println("Au revoir !");
+				break;
 			}
-		}	
+		}
 	}
 
 	/**
-	 * Print menu choices 
+	 * Print menu choices
 	 */
-	public static void displayMenu() {	
+	public static void displayMenu() {
 		System.out.println("\nQue souhaitez-vous faire ?");
-		System.out.println("1. ");
-		System.out.println("2. ");
-		System.out.println("3. ");
-		System.out.println("4. ");
-		System.out.println("5. ");
-		System.out.println("6. ");
-		System.out.println("7. Quitter l'application.");
+		System.out.println("[1] -- Afficher tous les articles en base. ");
+		System.out.println("[2] -- Afficher tous les articles (par pages). ");
+		System.out.println("[3] -- Gérer un article. ");
+		System.out.println("[4] -- Gérer une catégorie. ");
+		System.out.println("[5] -- Afficher tous les articles d'une catégorie ");
+		System.out.println("[6] --  Quitter l'application.");
 	}
-	
+
+	/**
+	 * Print CRUD choices
+	 */
+	public static void displayCRUDMenu(String name) {
+		System.out.println("\nQue souhaitez-vous faire ?");
+		System.out.println("[1] -- Ajouter " + name + ". ");
+		System.out.println("[2] -- Supprimer " + name + ". ");
+		System.out.println("[3] -- Mettre à jour " + name + ". ");
+		System.out.println("[4] -- Afficher le détail d'" + name + ". ");
+		System.out.println("[0] --  Retour à l'accueil.");
+	}
+
 	/**
 	 * Check if answer of user is an int
-	 * @return answer in scan 
+	 * 
+	 * @return answer in scan
 	 */
 	public static int scanInt() {
-		while(!scan.hasNextInt()) {
+		while (!scan.hasNextInt()) {
 			System.out.println("Merci de saisir un nombre.");
 			scan.next();
 		}
-		
+
 		return scan.nextInt();
+	}
+
+	/**
+	 * print all article from DB
+	 */
+	public static void displayArticles() {
+		String format = "%1$-4s | %2$-20s | %3$-30s | %4$-8s |\n";
+
+		System.out.println(String.join("", Collections.nCopies(73, "-")));
+		System.out.format(format, "REF", "DESCRIPTION", "MARQUE", "PRIX");
+		System.out.format(format, "----", String.join("", Collections.nCopies(20, "-")),
+				String.join("", Collections.nCopies(30, "-")), "--------");
+
+		 business.readArticles().forEach((n) ->
+			 System.out.format(format, n.getId(), n.getDescription(), n.getBrand(), 
+			 n.getPrice() + "€" )); 
+		 //+ "\n" + String.join("", Collections.nCopies(71, "-"))
+
+		System.out.println(String.join("", Collections.nCopies(73, "-")));
+	}
+	
+	/**
+	 * 
+	 */
+	public static void menuArticles() {
+		int choice = 5;
+		
+		while(choice != 0) {
+			displayCRUDMenu("un article");
+			choice = scanInt();
+			
+			switch (choice) {
+				case 0:
+					break;
+
+				case 1:
+					System.out.println("case 1");
+					break;
+					
+				case 2:
+					System.out.println("case 2");
+					break;
+					
+				case 3:
+					System.out.println("3");
+					break;
+					
+				case 4:
+					System.out.println('4');
+					break;
+			}
+		}
+	}
+
+	/**
+	 * print all articles from DB by page
+	 */
+	public static void displayArticlesByPage() {
+		
 	}
 }
--- SpringShopJpa/src/main/java/fr/fms/SpringShopJpaApplication.java
+++ SpringShopJpa/src/main/java/fr/fms/SpringShopJpaApplication.java
@@ -1,100 +1,100 @@
-package fr.fms;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import fr.fms.dao.ArticleRepository;
-import fr.fms.dao.CategoryRepository;
-import fr.fms.entities.Article;
-import fr.fms.entities.Category;
-
-@SpringBootApplication
-public class SpringShopJpaApplication implements CommandLineRunner  {
-	
-	@Autowired
-	private CategoryRepository categoryRepository;
-	
-	@Autowired
-	private ArticleRepository articleRepository;
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringShopJpaApplication.class, args);
-	}
-	
-	@Override
-	public void run(String... args) throws Exception {
-		//categoryRepository.save(new Category("Smartphone"));
-		//articleRepository.save(new Article("iPhone 3 - Lumière Stellaire", "Apple", 1209));
-		//articleRepository.save(new Article("S20", "Samsung", 250));
-		//articleRepository.save(new Article("S10+", "Samsung", 28));
-		
-//		for(Article article: articleRepository.findByBrand("Apple")) {
-//			System.out.println(article.getDescription());
-//		}
-//		for(Category cat: categoryRepository.findAll()) {
-//			System.out.println(cat.getName());
-//		}
+//package fr.fms;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//import fr.fms.dao.ArticleRepository;
+//import fr.fms.dao.CategoryRepository;
+//import fr.fms.entities.Article;
+//import fr.fms.entities.Category;
+//
+//@SpringBootApplication
+//public class SpringShopJpaApplication implements CommandLineRunner  {
+//	
+//	@Autowired
+//	private CategoryRepository categoryRepository;
+//	
+//	@Autowired
+//	private ArticleRepository articleRepository;
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(SpringShopJpaApplication.class, args);
+//	}
+//	
+//	@Override
+//	public void run(String... args) throws Exception {
+//		//categoryRepository.save(new Category("Smartphone"));
+//		//articleRepository.save(new Article("iPhone 3 - Lumière Stellaire", "Apple", 1209));
+//		//articleRepository.save(new Article("S20", "Samsung", 250));
+//		//articleRepository.save(new Article("S10+", "Samsung", 28));
 //		
-//		Article art1 = articleRepository.getArticleById((long) 1);
-//		System.out.println(art1);
-		
-//		for(Article article: articleRepository.findByBrandContains("a")) {
-//			System.out.println(article.getBrand());
-//		}
-//		System.out.println("-------");
-//		for(Article article: articleRepository.findByBrandAndPrice("Apple", 2500)) {
-//			System.out.println(article);
-//		}
-		
-//		Category smartphone = categoryRepository.save(new Category("Smartphone"));
-//		Category tablet = categoryRepository.save(new Category("Tablet"));
-//		Category pc = categoryRepository.save(new Category("PC"));
+////		for(Article article: articleRepository.findByBrand("Apple")) {
+////			System.out.println(article.getDescription());
+////		}
+////		for(Category cat: categoryRepository.findAll()) {
+////			System.out.println(cat.getName());
+////		}
+////		
+////		Article art1 = articleRepository.getArticleById((long) 1);
+////		System.out.println(art1);
 //		
-//		articleRepository.save(new Article("S10+", "Samsung", 350, smartphone));
-//		articleRepository.save(new Article("MI10", "Xiaomi", 100, smartphone));
+////		for(Article article: articleRepository.findByBrandContains("a")) {
+////			System.out.println(article.getBrand());
+////		}
+////		System.out.println("-------");
+////		for(Article article: articleRepository.findByBrandAndPrice("Apple", 2500)) {
+////			System.out.println(article);
+////		}
 //		
-//		articleRepository.save(new Article("GalaxyTab", "Samsung", 450, tablet));
-//		articleRepository.save(new Article("Ipad Pro", "Samsung", 850, tablet));
+////		Category smartphone = categoryRepository.save(new Category("Smartphone"));
+////		Category tablet = categoryRepository.save(new Category("Tablet"));
+////		Category pc = categoryRepository.save(new Category("PC"));
+////		
+////		articleRepository.save(new Article("S10+", "Samsung", 350, smartphone));
+////		articleRepository.save(new Article("MI10", "Xiaomi", 100, smartphone));
+////		
+////		articleRepository.save(new Article("GalaxyTab", "Samsung", 450, tablet));
+////		articleRepository.save(new Article("Ipad Pro", "Samsung", 850, tablet));
+////		
+////		articleRepository.save(new Article("R510", "Asus", 600, pc));
 //		
-//		articleRepository.save(new Article("R510", "Asus", 600, pc));
-		
-//		for(String article: articleRepository.findByCategoryId((long) 4)) {
-//			System.out.println(article);
+////		for(String article: articleRepository.findByCategoryId((long) 4)) {
+////			System.out.println(article);
+////		}
+//		
+//		//EXO 1.2
+//		System.out.println("Exo 1.2");
+//		System.out.println(articleRepository.getArticleById((long) 1));
+//		System.out.println(articleRepository.findById((long) 1));
+//		System.out.println("------------------");
+//		
+//		//EXO 1.3
+//		System.out.println("Exo 1.3");
+//		System.out.println(articleRepository.findByDescriptionAndBrand("MacBook", "Apple"));
+//		System.out.println("--------");
+//
+//		//EXO 1.4
+//		//articleRepository.deleteById((long) 5);
+//		
+//		//EXO 1.5
+//		System.out.println("Exo 1.5");
+//		Category smartphone = categoryRepository.findByName("Smartphone");
+//		articleRepository.save(new Article((long) 3, "iPhone 13 - Lumière Stellaire", "Apple", 1209, smartphone));
+//		//articleRepository.saveOrUpdate(new Article((long) 3, "iPhone 13 - Lumière Stellaire", "Apple", 1209, smartphone));
+//		System.out.println("--------");
+//		
+//		//EXO 1.6
+//		System.out.println("Exo 1.6");
+//		for(Category cat : categoryRepository.findAllByOrderByNameDesc()){
+//			System.out.println(cat);
 //		}
-		
-		//EXO 1.2
-		System.out.println("Exo 1.2");
-		System.out.println(articleRepository.getArticleById((long) 1));
-		System.out.println(articleRepository.findById((long) 1));
-		System.out.println("------------------");
-		
-		//EXO 1.3
-		System.out.println("Exo 1.3");
-		System.out.println(articleRepository.findByDescriptionAndBrand("MacBook", "Apple"));
-		System.out.println("--------");
-
-		//EXO 1.4
-		//articleRepository.deleteById((long) 5);
-		
-		//EXO 1.5
-		System.out.println("Exo 1.5");
-		Category smartphone = categoryRepository.findByName("Smartphone");
-		articleRepository.save(new Article((long) 3, "iPhone 13 - Lumière Stellaire", "Apple", 1209, smartphone));
-		//articleRepository.saveOrUpdate(new Article((long) 3, "iPhone 13 - Lumière Stellaire", "Apple", 1209, smartphone));
-		System.out.println("--------");
-		
-		//EXO 1.6
-		System.out.println("Exo 1.6");
-		for(Category cat : categoryRepository.findAllByOrderByNameDesc()){
-			System.out.println(cat);
-		}
-		System.out.println("--------");
-		for(Category cat : categoryRepository.findAllByOrderByNameAsc()){
-			System.out.println(cat);
-		}
-		System.out.println("--------");
-	}
-	
-}
+//		System.out.println("--------");
+//		for(Category cat : categoryRepository.findAllByOrderByNameAsc()){
+//			System.out.println(cat);
+//		}
+//		System.out.println("--------");
+//	}
+//	
+//}
